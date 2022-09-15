c9aad0e7e49bb681f1a0430e7ebd41c66648ea85
atilabarbosa <atilacabo@gmail.com>
2022-09-15 16:01:23 -0300

    exercicio 1509

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -4,3 +4,34 @@ const objeto = {
     username: "astrodev_labenu",
     senha: "melhorDeTodos"
 }
+
+//Crie duas funcões que recebem como parâmetro um objeto.
+//1. A primeira deve retornar o objeto, alterando as propriedades dele para que fiquem em CAIXA ALTA.
+// function caixaAlta (elemento){
+//     elemento.nome = elemento.nome.toUpperCase()
+//     elemento.profissao = elemento.profissao.toUpperCase()
+//     elemento.username = elemento.username.toUpperCase()
+//     elemento.senha = elemento.senha.toUpperCase()
+//     return elemento
+// }
+
+function caixaAlta(elemento){
+    for(let i in elemento){
+        elemento[i] = elemento[i].toUpperCase()
+    }
+}
+
+console.log(caixaAlta(objeto))
+
+//A segunda deve retornar os valores do objeto como texto corrido.
+function textoCorrigido(elemento){
+    let text = ""
+    for(let i in elemento){
+        text += ` ${i} ${elemento[i]}`
+    }
+    return text.trim()
+}
+console.log(textoCorrigido(objeto))
+
+
+//Em seguida, crie uma funcão que recebe um objeto e um callback como parâmetros. A funcão deve passar o objeto como argumento da funcão de callback, e em seguida imprimir o valor retornado.
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -11,3 +11,8 @@ const produtos = [
     { nome: "Sabão em Pó", categoria: "Limpeza", preco: 10.80 }
  ]
  
+ //retorne um novo array com o nome dos produtos da categoria Limpeza.
+const limpeza = produtos.filter((elemento) => {
+    return elemento.categoria === "Limpeza"
+})    
+ console.log(limpeza)
